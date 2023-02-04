7c79c37541c53b8081d5b822c1352c938114e710
Abhijit Parashare <Abhijit.Parashare@gmail.com>
2023-02-04 23:00:24 +0530

    added wsgi file

---
+++ flask-app/application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = application = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return 'Welcome to flask app'
+
+if __name__ == "__main__":
+   application.run()
